5ae1973898a6938c755cb147fd81d5369520283f
dym0080 <308960474@qq.com>
2019-10-25 11:06:50 +0800

    update

--- fluent-python-code/test.py
+++ fluent-python-code/test.py
@@ -1,21 +1,17 @@
-import bisect
-import sys
-HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
-NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
-ROW_FMT = '{0:2d} @ {1:2d} {2}{0:<2d}'
+
+def clip(text, max_len=80):
+ 
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+        if space_after >= 0:
+            end = space_after
+    if end is None: # 没找到空格
+        end = len(text)
+    return text[:end].rstrip()
 
 
-def demo(bisect_fn):
-    for needle in reversed(NEEDLES):
-        position = bisect_fn(HAYSTACK, needle)
-        offset = position * ' |'
-        print(ROW_FMT.format(needle, position, offset))
-
-if __name__ == '__main__':
-    if sys.argv[-1] == 'left':
-        bisect_fn = bisect.bisect_left
-    else:
-        bisect_fn = bisect.bisect
-        print('DEMO:', bisect_fn.__name__)
-        print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
-        demo(bisect_fn)
