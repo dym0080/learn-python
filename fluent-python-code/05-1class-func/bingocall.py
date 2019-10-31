e6ec7cf80646c7647f709ec2dd108bbc659dadef
dym0080 <308960474@qq.com>
2019-10-31 17:33:04 +0800

    update fluent-python-code

---
+++ fluent-python-code/05-1class-func/bingocall.py
@@ -0,0 +1,18 @@
+import random
+
+class BingoCage:
+
+    def __init__(self, items):
+        self._items = list(items)  # <1>
+        random.shuffle(self._items)  # <2>
+
+    def pick(self):  # <3>
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')  # <4>
+
+    def __call__(self):  # <5>
+        return self.pick()
+
+
